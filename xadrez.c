0dd17c65a4981211ff9801e0e6957ce153963100
Cauandev01 <cauanjorge76@gmail.com>
2025-04-02 16:13:43 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,52 +1,54 @@
 #include <stdio.h>
 
-int main(){
-    
-    printf("Torre:\n");
-
-    for (int i = 0; i < 5; i++){
-        printf("Direita.\n"); // move a torre para a direita
+// Função recursiva para mover a Torre para a direita
+void moverTorre(int movimentos) {
+    if (movimentos > 0) { 
+        printf("Direita\n");
+        moverTorre(movimentos - 1);
     }
+}
 
-    printf("----------------------------------\n");
+// Função recursiva para mover o Bispo (com loops aninhados)
+void moverBispo(int movimentos, int passo) {
+    if (movimentos > 0) {
+        for (int i = 0; i < passo; i++) { 
+            printf("Cima, Direita\n");
+        }
+        moverBispo(movimentos - 1, passo);
+    }
+}
+
+// Função recursiva para mover a Rainha para a esquerda
+void moverRainha(int movimentos) {
+    if (movimentos > 0) {
+        printf("Esquerda\n");
+        moverRainha(movimentos - 1);
+    }
+}
+
+// Função para mover o Cavalo 
+void moverCavalo() {
+    printf("Cima\n");
+    printf("Cima\n");
+    printf("Direita\n");
+}
+
+int main() {
+    printf("Torre:\n");
+    moverTorre(5);
+    printf("\n----------------------------------\n");
 
     printf("Bispo:\n");
-
-    int b = 0;
-
-    while (b < 5)
-    {
-        printf("Cima, direita.\n"); // move o bispo para cima e direita
-        b++;
-    }
-
-    printf("----------------------------------\n");
+    moverBispo(5, 1);
+    printf("\n----------------------------------\n");
 
     printf("Rainha:\n");
+    moverRainha(8);
+    printf("\n----------------------------------\n");
 
-    int r = 0;
-
-    do
-    {
-        printf("Esquerda.\n"); // move a Rainha para a esquerda
-        r++;
-    } while (r < 8);
-
-    printf("----------------------------------\n");
-
-     printf("Cavalo:\n");
-    int movimento = 1; // Para controlar o movimento em L
-
-    for (; movimento > 0; movimento--){
-        for(int i = 0; i < 2; i++){ 
-            printf("Baixo\n"); // Imprime 'Baixo' duas vezes
-        }
-        printf("Esquerda\n"); // Imprime 'Esquerda' uma vez
-    }
+    printf("Cavalo:\n");
+    moverCavalo(); 
 
     return 0;
-    
-
-
-    
 }
+
